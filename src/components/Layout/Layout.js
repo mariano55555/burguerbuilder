8eccc112bd22be9f68254f65a2679e752b3f6016
Jorge Mariano Paz Flores <jorge.sv@squeaky.com>
2019-10-04 14:59:23 -0600

    sidedrawer, tooldbar, navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Auxiliar from '../../hoc/Auxiliar';
 
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Auxiliar>
-        <div>Tooldbar, SideDrawer, Backdrop</div>
+       <Toolbar />
+       <SideDrawer />
         <main className={classes.Content}>
             { props.children }
         </main>
